c9cf7b3b2ecc5b51e4c266adddca057d46c24750
zhaozongchang <xingyingsuifeng@gmail.com>
2017-10-12 21:32:40 +0800

    step 7 upload resume

--- app/models/job.rb
+++ app/models/job.rb
@@ -4,7 +4,7 @@ class Job < ApplicationRecord
   validates :wage_upper_bound, presence: true
   validates :wage_lower_bound, presence: true
   validates :wage_lower_bound, numericality: { greater_than: 0}
-
+  has_many :resumes
    def publish!
      self.is_hidden = false
      self.save
